3b23782d9bbbc67bc2d3f4a1feec11cc5b40cfd3
Шеваков Александр Сергеевич <assheva1@mts.ru>
2022-04-05 21:59:16 +0300

    6 Реализуйте класс Point

--- DelimetersCalculator.dart
+++ DelimetersCalculator.dart
@@ -126,6 +126,40 @@ class WortToNumberConverter {
   }
 }
 
+class Point {
+  final double x;
+  final double y;
+  final double z;
+
+  Point(this.x, this.y, this.z);
+
+  factory Point.zero() {
+    return Point(0, 0, 0);
+  }
+
+  factory Point.vector() {
+    return Point(1, 1, 1);
+  }
+
+  double distanceTo(Point another) {
+    return sqrt(pow((another.x - x), 2) +
+        pow((another.y - y), 2) +
+        pow((another.z - z), 2));
+  }
+
+  double square(Point one, Point two, Point three) {
+    double firstSide = one.distanceTo(two);
+    double secondSide = two.distanceTo(three);
+    double thirdstSide = three.distanceTo(one);
+    double halfPerimeter = (firstSide + secondSide + thirdstSide) / 2;
+
+    return sqrt(halfPerimeter *
+        (halfPerimeter - firstSide) *
+        (halfPerimeter - secondSide) *
+        (halfPerimeter - thirdstSide));
+  }
+}
+
 void main() {
   print(WortToNumberConverter.getSetFromString('one, two, three, cat, dog'));
 }
