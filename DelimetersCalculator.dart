744b168fb0b7b281474ef29a6273f674d0b9cc58
Шеваков Александр Сергеевич <assheva1@mts.ru>
2022-04-06 12:44:04 +0300

    9 Вычисление интеграла

--- DelimetersCalculator.dart
+++ DelimetersCalculator.dart
@@ -236,6 +236,22 @@ class UserManager<T extends User> {
   }
 }
 
+class MidpointRectangleRule {
+  double calculationIntegral(Function func, double a, double b, int nseg) {
+    double dx = 1.0 * (b - a) / nseg;
+    double sum = 0.0;
+    final list = new List<int>.generate(nseg, (i) => i + 1);
+
+    double xstart = a + 0.5 * dx;
+
+    for (var item in list) {
+      sum += func(xstart + item * dx);
+    }
+
+    return sum * dx;
+  }
+}
+
 void main() {
   final admin = new AdminUser('test@mail.com');
   final mana = new UserManager();
