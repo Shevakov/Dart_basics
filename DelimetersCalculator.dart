f2910c6019c9ddc67a196f9623b14f55c96ba8f4
Шеваков Александр Сергеевич <assheva1@mts.ru>
2022-04-06 09:34:14 +0300

    8 Создайте класс User, у которого есть поле email.

--- DelimetersCalculator.dart
+++ DelimetersCalculator.dart
@@ -196,6 +196,50 @@ extension on num {
   }
 }
 
+class User {
+  String email;
+
+  User(this.email);
+}
+
+mixin MailSystem on User {
+  String getMailSystem() => email.substring(email.indexOf('@') + 1);
+}
+
+class AdminUser extends User with MailSystem {
+  AdminUser(String email) : super(email);
+}
+
+class GeneralUser extends User {
+  GeneralUser(String email) : super(email);
+}
+
+class UserManager<T extends User> {
+  final users = <User>[];
+
+  void addUser(User user) {
+    users.add(user);
+  }
+
+  void clearUsers() {
+    users.clear();
+  }
+
+  void printEmails() {
+    for (var user in users) {
+      if (user is AdminUser) {
+        print(user.getMailSystem());
+      } else {
+        print(user.email);
+      }
+    }
+  }
+}
+
 void main() {
-  print(5.root(4.0, 2.0));
+  final admin = new AdminUser('test@mail.com');
+  final mana = new UserManager();
+  mana.addUser(admin);
+  mana.addUser(new User('123@123.ru'));
+  mana.printEmails();
 }
