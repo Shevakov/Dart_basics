f058690256bd87e25007ccbbfe70fe5a19036b03
Шеваков Александр Сергеевич <assheva1@mts.ru>
2022-04-06 08:50:58 +0300

    7 Реализуйте метод, который вычисляет корень любой заданной степени из числа.

--- DelimetersCalculator.dart
+++ DelimetersCalculator.dart
@@ -1,4 +1,3 @@
-import 'dart:io';
 import 'dart:math';
 
 class DelimetersCalculator {
@@ -160,6 +159,43 @@ class Point {
   }
 }
 
+double f_prime(double x, double p) {
+  return p * power(x, p - 1);
+}
+
+double f(double x, double p, double num) {
+  return power(x, p) - num;
+}
+
+double power(double x, double y) {
+  if (y == 0) return 1;
+  if (y % 2 == 0)
+    return power(x, y / 2) * power(x, y / 2);
+  else
+    return x * power(x, y / 2) * power(x, y / 2);
+}
+
+extension on num {
+  double root(double num, double p) {
+    double left = -num;
+    double right = num;
+
+    double x;
+    while (true) {
+      x = (left + right) / 2.0;
+      double value = f(x, p, num);
+      double prime = f_prime(x, p);
+      if (value * prime <= 0)
+        left = x;
+      else
+        right = x;
+      if (value < 0.000001 && value >= 0) {
+        return x;
+      }
+    }
+  }
+}
+
 void main() {
-  print(WortToNumberConverter.getSetFromString('one, two, three, cat, dog'));
+  print(5.root(4.0, 2.0));
 }
